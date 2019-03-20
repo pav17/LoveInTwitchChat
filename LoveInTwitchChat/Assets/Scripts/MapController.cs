cb799c3d0cd00300ef057eeabebac8b789ee0ca2
pav17 <pav17@hampshire.edu>
2019-03-20 15:48:20 -0400

    Initial placeholder assets

---
+++ LoveInTwitchChat/Assets/Scripts/MapController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Park()
+    {
+        //create prefab for scenes
+    }
+
+    public void Cafe()
+    {
+
+    }
+
+    public void Mall()
+    {
+
+    }
+}
