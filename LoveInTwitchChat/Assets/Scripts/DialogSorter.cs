b786c0316f94864d47787b85dd8c6361fc382b8a
pav17 <pav17@hampshire.edu>
2019-03-31 23:37:48 -0400

    Fixed some bugs

--- LoveInTwitchChat/Assets/Scripts/DialogSorter.cs
+++ LoveInTwitchChat/Assets/Scripts/DialogSorter.cs
@@ -39,13 +39,19 @@ public class DialogSorter : MonoBehaviour
         Queue<string> Cafe1 = new Queue<string>();
         Cafe1.Enqueue("You walk into the cafe and get in line to order at the counter, when you notice Positive at the head of the line doing " +
             "their best to try to motivate the barista who, apparently, looked a little demotivated.");
-        Cafe1.Enqueue("Hey, no need to look so down, you’re doing a great job!");
-        Cafe1.Enqueue("Oh, thanks. I was getting pretty worn out, but your positivity has helped me feel better!");
-        Cafe1.Enqueue("That’s the spirit!");
-        Cafe1.Enqueue("(Thinks: Wow, I guess a bit of positivity really can go a long way!)");
+        Cafe1.Enqueue("Positive: Hey, no need to look so down, you’re doing a great job!");
+        Cafe1.Enqueue("Barista: Oh, thanks. I was getting pretty worn out, but your positivity has helped me feel better!");
+        Cafe1.Enqueue("Positive: That’s the spirit!");
+        Cafe1.Enqueue("Me: (Thinks: Wow, I guess a bit of positivity really can go a long way!)");
         Cafe1.Enqueue("END");
-
         CafeConvo.Enqueue(Cafe1);
+
+        Queue<string> Cafe2 = new Queue<string>();
+        Cafe2.Enqueue("This is a test sentance.");
+        Cafe2.Enqueue("END");
+        CafeConvo.Enqueue(Cafe2);
+
+        
         
         return (CafeConvo);
     }
--- LoveInTwitchChat/Assets/Scripts/TextboxController.cs
+++ LoveInTwitchChat/Assets/Scripts/TextboxController.cs
@@ -8,16 +8,20 @@ public class TextboxController : MonoBehaviour
 
     Text sceneDialog;
     Queue<string> sceneText;
+    bool continueFlag;
+    bool exitFlag;
 
-    void Start()
+    void Awake()
     {
         sceneDialog = GetComponent<Text>();
     }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Return))
+        //if (Input.GetKeyUp(KeyCode.Return))
+        if (continueFlag)
         {
+            continueFlag = false;
             string currentText = sceneText.Dequeue();
             if (currentText != "END")
             {
@@ -30,11 +34,27 @@ public class TextboxController : MonoBehaviour
                 //spawn player response
             }
         }
+
+        if (exitFlag)
+        {
+            Destroy(GameObject.Find("PlaceholderBackground(Clone)"));
+            Destroy(GameObject.FindGameObjectWithTag("Dialog"));
+        }
     }
 
     public void CreateConversation(Queue<string> newConversation)
     {
         sceneText = newConversation;
         sceneDialog.text = sceneText.Dequeue();
+    }
+
+    public void Continue()
+    {
+        continueFlag = true;
+    }
+
+    public void Exit()
+    {
+        exitFlag = true;
     }
 }
