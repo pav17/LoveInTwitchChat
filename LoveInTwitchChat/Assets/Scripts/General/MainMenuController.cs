cfa73acb29cde0e7646a96b09c092cf65dc5f2b8
pav17 <pav17@hampshire.edu>
2019-04-29 14:11:07 -0400

    Map and more menu stuff. Currently broken

--- LoveInTwitchChat/Assets/Scripts/General/MainMenuController.cs
+++ LoveInTwitchChat/Assets/Scripts/General/MainMenuController.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
+        SceneManager.LoadScene(sceneName: "MainMap");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Options()
     {
-        
+        SceneManager.LoadScene(sceneName: "Options");
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(sceneName: "Tutorial");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
