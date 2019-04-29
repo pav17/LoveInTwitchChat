bb6cc65a79fb6ab2c7554d4e5b6fffdde2896dd7
pav17 <pav17@hampshire.edu>
2019-04-29 15:49:54 -0400

    updated menus. options is currently broken

---
+++ LoveInTwitchChat/Assets/Scripts/Menu/OptionsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    public Text usernamePlaceholder;
+    public Text username;
+    public Text pollTimer;
+    public Text pollPlaceholder;
+
+    void Start()
+    {
+        pollPlaceholder.text = Global.global.pollTimer.ToString();
+    }
+    void Update()
+    {
+        if (Global.global.twitchName != "")
+        {
+            usernamePlaceholder.text = Global.global.twitchName;
+        }
+        else
+        {
+            usernamePlaceholder.text = "Twitch Username";
+        }
+    }
+
+    public void OnUsernameChange()
+    {
+        if (username.text != "")
+        {
+            Global.global.twitchName = username.text;
+        }
+        Global.global.pollTimer = pollTimer.text;
+    }
+}
